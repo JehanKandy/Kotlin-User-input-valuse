6cf62709a95140dc4f0bde6157f1717b16f05112
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2021-12-18 08:15:43 +0530

    Add files via upload

---
+++ input_print.kt
@@ -0,0 +1,14 @@
+
+//print user input values
+//impport java scanner
+import java.util.Scanner  
+
+fun main() {  
+    //set the scanner
+    val read = Scanner(System.`in`)  
+    println("Enter your age")
+    
+    //get the input enterded by user
+    var age = read.nextInt()  
+    println("Your input age is "+age)  
+}  
